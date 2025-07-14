721f585e863ea3262471c4060003703898b76622
IgorStepanov <wazar@mail.ru>
2025-07-14 11:20:47 +0300

    implement goods generator (#7)
    * implement goods generator
    
    * fix for 1.5
    
    ---------
    
    Co-authored-by: blackmark3000 <115068977+blackmark3000@users.noreply.github.com>

--- Program/interface/questbook.c
+++ Program/interface/questbook.c
@@ -1313,7 +1313,7 @@ void FillPriceList(string _tabName, string  attr1)
 		if(StoreNum > 0)
 		{
 			refStorage = &stores[StoreNum];
-			for (i = 0; i < GOODS_QUANTITY; i++) 
+			for (i = 0; i < GetArraySize(&Goods); i++) 
 			{
 				row = "tr" + n;
 				sGoods = "Gidx" + i;	
@@ -1425,7 +1425,7 @@ void TradebookFillPriceList(string _tabName, string  attr1)
 	{
 	    // <--
 	    n = 1;
-	    for (i = 0; i < GOODS_QUANTITY; i++)
+	    for (i = 0; i < GetArraySize(&Goods); i++)
 	    {
 	        row = "tr" + n;
 	        sGoods = "Gidx" + i;			
